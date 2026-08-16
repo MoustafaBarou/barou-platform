84f6d011e4e70522f8a5ab3f897c7f6c802822e7
Moustafa Barou <m.barou@barouconsulting.nl>
2026-08-16 16:07:51 +0000

    feat: provision Gitea and Jenkins platform infrastructure

--- infrastructure/proxmox/variables.tf
+++ infrastructure/proxmox/variables.tf
@@ -1,5 +1,5 @@
 variable "proxmox_node_name" {
-  description = "Name of the Proxmox node where the VM will be created."
+  description = "Name of the Proxmox node where virtual machines are created."
   type        = string
   default     = "pve"
 }
@@ -10,44 +10,14 @@ variable "template_vm_id" {
   default     = 9000
 }
 
-variable "vm_id" {
-  description = "VM ID assigned to the new virtual machine."
-  type        = number
-  default     = 103
-}
-
-variable "vm_name" {
-  description = "Name of the virtual machine."
-  type        = string
-  default     = "ubuntu-tf-01"
-}
-
-variable "vm_description" {
-  description = "Description shown in the Proxmox web interface."
-  type        = string
-  default     = "Ubuntu VM managed by Terraform"
-}
-
-variable "cpu_cores" {
-  description = "Number of virtual CPU cores assigned to the VM."
-  type        = number
-  default     = 2
-}
-
-variable "memory_mb" {
-  description = "Amount of dedicated memory assigned to the VM in MiB."
-  type        = number
-  default     = 2048
-}
-
 variable "network_bridge" {
-  description = "Proxmox network bridge connected to the VM."
+  description = "Proxmox network bridge connected to virtual machines."
   type        = string
   default     = "vmbr0"
 }
 
 variable "datastore_id" {
-  description = "Proxmox datastore used for the Cloud-Init disk."
+  description = "Proxmox datastore used for Cloud-Init."
   type        = string
   default     = "local-lvm"
 }
@@ -59,7 +29,41 @@ variable "cloud_init_username" {
 }
 
 variable "ssh_public_key_path" {
-  description = "Local path to the SSH public key added to the VM through Cloud-Init."
+  description = "Local path to the SSH public key added to virtual machines through Cloud-Init."
   type        = string
   default     = "~/.ssh/id_ed25519.pub"
+}
+
+variable "virtual_machines" {
+  description = "Ubuntu virtual machines managed by Terraform."
+
+  type = map(object({
+    vm_id       = number
+    description = string
+    cpu_cores   = number
+    memory_mb   = number
+  }))
+
+  default = {
+    ubuntu-tf-01 = {
+      vm_id       = 103
+      description = "Terraform-managed Ubuntu automation target"
+      cpu_cores   = 2
+      memory_mb   = 2048
+    }
+
+    gitea-01 = {
+      vm_id       = 104
+      description = "Self-hosted Git service managed by Terraform"
+      cpu_cores   = 2
+      memory_mb   = 2048
+    }
+
+    jenkins-01 = {
+      vm_id       = 105
+      description = "CI automation server managed by Terraform"
+      cpu_cores   = 2
+      memory_mb   = 3072
+    }
+  }
 }
